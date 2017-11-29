ca6beccc1e4efa6bdc9002e3ca8550babd60b049
Your Name <you@example.com>
2017-11-29 20:42:13 +0800

    2017-11-11-29

---
+++ app/src/main/java/com/a35d6c/test/mytest/Main2Activity.java
@@ -0,0 +1,17 @@
+package com.a35d6c.test.mytest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.a36d5c.test1.test112.R;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+    }
+
+}
---
+++ app/src/main/java/com/a36d5c/test1/test112/MainActivity.java
@@ -0,0 +1,15 @@
+package com.a36d5c.test1.test112;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+    }
+
+}
